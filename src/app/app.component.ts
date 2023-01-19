e74d7739140f8da6ac6c488f92993bed1bdf1888
sarahzull <85895574+sarahzull@users.noreply.github.com>
2023-01-20 00:05:00 +0800

    some update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'simple-address-book-dcm';
+  name = "";
+  phone = "";
+  email = "";
+
+  title = 'Simple Address Book';
 }
